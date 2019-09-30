6c337a3701d4c556a9899b4778dd0361ecadf90b
Antonio <super_tony123@hotmail.com>
2019-09-29 19:50:29 -0500

    update 2

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 let arreglo = [];
+var personas = {};
 
 app.get("/", ( req,res) =>{
 	res.send("Hellow world");
@@ -10,7 +11,11 @@ app.listen(3000, () => {
 	console.log("Server is running...");
 });
 
-app.get("/:nombre /:edad", (req,res)=>{
+app.get("/:id", (req,res)=>{
     arreglo.push(req.params.id);
     console.log(arreglo);
 });
+
+app.get("/:nombre/:edad",(nombre,))=>{
+	personas.nombre = (nombre,edad)
+};
